b8734aef4efed48e28d25f7c0b28ce0a6089ee17
Alexander Goldberg <algold@gmail.com>
2013-08-21 12:25:15 +0300

    Add automount fix

--- osx_tweaks.sh
+++ osx_tweaks.sh
@@ -2,3 +2,9 @@
 
 # Enable text selection in preview
 defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+
+# Install macfuse and sshfs and make automount work
+brew install macfuse sshfs
+sudo ln -s /usr/local/bin/sshfs /sbin/mount_sshfs
+sudo automount -c
+sudo -s echo '#dummy:root@alexg-centos:/home/alexg /sshfs/alexg-centos sshfs volname=volname,noforget,uid=501,gid=20,allow_other 0 0' >> /etc/fstab
