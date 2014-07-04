e2029ee31e56ebba05ff33517471651bce3e46d3
Alexander Goldberg <alexg@infinidat.com>
2014-07-04 23:42:55 +0300

    Small changes

--- osx_tweaks.sh
+++ osx_tweaks.sh
@@ -4,8 +4,8 @@
 defaults write com.apple.finder QLEnableTextSelection -bool TRUE
 
 # Install macfuse and sshfs and make automount work
-brew install fuse4x fuse4x-kext sshfs
-sudo sysctl -w vfs.generic.fuse4x.tunables.admin_group=20
-sudo ln -s /usr/local/bin/sshfs /sbin/mount_sshfs
-sudo automount -c
-sudo -s echo '#dummy:root@alexg-centos:/home/alexg /sshfs/alexg-centos sshfs volname=volname,noforget,uid=501,gid=20,allow_other 0 0' >> /etc/fstab
+#brew install fuse4x fuse4x-kext sshfs
+#sudo sysctl -w vfs.generic.fuse4x.tunables.admin_group=20
+#sudo ln -s /usr/local/bin/sshfs /sbin/mount_sshfs
+#sudo automount -c
+#sudo -s echo '#dummy:root@alexg-centos:/home/alexg /sshfs/alexg-centos sshfs volname=volname,noforget,uid=501,gid=20,allow_other 0 0' >> /etc/fstab
